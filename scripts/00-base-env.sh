c828d4dda25524f142c138e6533663520bab2b32
a5467021 <32480340+a5467021@users.noreply.github.com>
2022-01-03 10:15:31 +0800

    modify sshd settings to speedup configuration

--- scripts/00-base-env.sh
+++ scripts/00-base-env.sh
@@ -5,5 +5,13 @@ sed -e 's@dl.rockylinux.org/$contentdir@mirrors.sjtug.sjtu.edu.cn/rocky@g' -i /e
 # dnf -y update
 dnf -y install epel-release yum-utils podman
 
+# epel mirror
+sed -e 's!^metalink=!#metalink=!g' \
+    -e 's!^#baseurl=!baseurl=!g' \
+    -e 's!//download\.fedoraproject\.org/pub!//mirrors.tuna.tsinghua.edu.cn!g' \
+    -e 's!//download\.example/pub!//mirrors.tuna.tsinghua.edu.cn!g' \
+    -e 's!http://mirrors!https://mirrors!g' \
+    -i /etc/yum.repos.d/epel*.repo
+
 # Docker repo for CentOS 8; however unnecessary as ceph-ansible hardcodes to use padman, which is shipped with CentOS 8 as default.
 # yum-config-manager --add-repo https://mirror.tuna.tsinghua.edu.cn/docker-ce/linux/centos/docker-ce.repo
